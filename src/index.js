004154787fdae53f83ab7529a63e4e4e64a57152
Radosław Łuckoś <luckos.rr@hotmail.com>
2021-04-28 14:30:38 +0200

    Made logged state passed to Redux Store after login

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from './state/reducers'
 import './styles/index.scss';
 import App from './components/App';
+import thunk from "redux-thunk" ;
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+const store = createStore(allReducers,composeWithDevTools(
+  applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
