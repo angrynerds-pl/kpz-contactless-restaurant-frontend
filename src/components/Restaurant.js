93d93028ff319f46f611d9327b0e00bf4b95283e
Radosław Łuckoś <luckos.rr@hotmail.com>
2021-05-17 21:06:56 +0200

    restaurant list styles corrected

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -3,18 +3,14 @@ import '../styles/restaurant-list.scss'
 const Restaurant = (props) => {
 
     return (
-        <li className="all-restaurants__restaurant">
-            <div>
-                <img className="all-restaurants__resimage" src='../image/1.jpg' alt={props.name} />   
-            </div>
-            <div className="all-restaurants__resdata">
-                <h2>{props.name}</h2>
-                <address>{`ul. ${props.street} ${props.streetNr}, ${props.city}`}</address>
-            </div>
-            <div className="all-restaurants__buttons">
-                <button className="all-restaurants__editdata">Edytuj dane restauracji</button> 
-                <button className="all-restaurants__editmenu">Edytuj menu restauracji</button> 
-            </div>
+        <li className="all-restaurants__restaurant restaurant">
+            
+            <img className="restaurant__image" src='../image/1.jpg' alt={`${props.name}-logo`} />   
+            <h2 className='restaurant__name'>{props.name}</h2>
+            <address className='restaurant__address'>{`ul. ${props.street} ${props.streetNr}, ${props.city}`}</address>
+            <button className="restaurant__btn edit_data">Edytuj dane restauracji</button> 
+            <button className="restaurant__btn edit_menu">Edytuj menu restauracji</button> 
+            
         </li>
     );
 }
